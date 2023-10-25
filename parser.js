f2ba3aa1d053d186d3d1825a325495cdec434f30
JazzKan <127630596+JazzKan@users.noreply.github.com>
2023-10-25 23:23:54 +0530

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,17 +1,6 @@
-var parser = require('xml-js');
-
-const xml = require('fs').readFileSync('junit.xml', 'utf-8');
-
-const options = {compact: true, ignoreComment: true, spaces: 4};
-
-const result = parser.xml2json(xml, options)
-
-const parsedResult = JSON.stringify(result, '');
-
-console.log(parsedResult);
 
 class API {
-  construcor(url, method = 'GET')
+  constructor(url, method = 'GET')
   {
     this.url = url;
     this.method = method;
