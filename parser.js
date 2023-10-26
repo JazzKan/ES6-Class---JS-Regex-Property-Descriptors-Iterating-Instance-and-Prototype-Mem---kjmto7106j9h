ab59c6b193f6b466ea1a528e9a2a775faf51fcb3
JazzKan <127630596+JazzKan@users.noreply.github.com>
2023-10-26 18:17:55 +0530

    Update parser.js

--- parser.js
+++ parser.js
@@ -9,19 +9,3 @@ const result = parser.xml2json(xml, options)
 const parsedResult = JSON.stringify(result, '');
 
 console.log(parsedResult);
-
-class API {
-  constructor(url, method = 'GET')
-  {
-    this.url = url;
-    this.method = method;
-    this.#secure = url.startsWith("https")
-  }
-  isSecure(){
-    return this.#secure
-  }
-  updateURL(url) {
-    this.url = url
-    this.#secure = url.startsWith("https")
-  }
-}
